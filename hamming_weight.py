a2fd0617db3c45e449d3f5620e3c6ae84682bec0
Gabby Vaillant <gabbyvaillant@icloud.com>
2022-11-04 10:35:14 -0400

    Update hamming_weight.py

--- hamming_weight.py
+++ hamming_weight.py
@@ -20,39 +20,3 @@ def hamming(n):
 
 print(hamming(95601))
 print(bin(95601)[2:])
-
-# Notes:
-# Iterate through digits in a number
-# need to make 0 a str by putting quotes around it
-
-
-
-# dave's way
-
-#!/usr/bin/env python3
-# hamming_weight_instructor.py
-
-# See https://en.wikipedia.org/wiki/Hamming_weight
-
-
-def pop_count(n):
-    count_onebits = 0
-    while n > 0:
-        count_onebits += n % 2 # what the remainder mod 2
-        n //= 2  # divide by two each time same as n = n // 2
-    return count_onebits
-
-
-def main():
-    n = 95601
-
-    hw = pop_count(n)
-
-    print(f"The Hamming weight of = {n:,}" f" in base 2 is {hw:,}")
-
-    hw = bin(n).count("1")  # count the number of times "1" appears in str. iter through str
-    print(f"The Hamming weight of = {n:,}" f" in base 2 is {hw:,}")
-
-
-if __name__ == "__main__":
-    main()
